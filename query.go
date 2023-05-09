a8a9517cca4c151bfb8d8d813c99ced12a0a5991
linshenqi <linshenqi@outlook.com>
2023-05-09 17:38:02 +0800

    1. add query base

---
+++ query.go
@@ -0,0 +1,88 @@
+package sptty
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/kataras/iris/v12"
+	"gorm.io/gorm"
+)
+
+const (
+	DefaultPageSize = 20
+)
+
+type IQuery interface {
+	fromCtx(ctx iris.Context)
+	loadDB(db *gorm.DB)
+	ToQuery(paging bool) *gorm.DB
+}
+
+type QueryBase struct {
+	IQuery
+
+	db *gorm.DB
+
+	Page     int64
+	PageSize int64
+	IDs      []string
+}
+
+func (s *QueryBase) loadDB(db *gorm.DB) {
+	s.db = db
+}
+
+func (s *QueryBase) fromCtx(ctx iris.Context) {
+	ids := ctx.URLParam("IDs")
+	if ids != "" {
+		s.IDs = strings.Split(ids, ",")
+	}
+
+	page, err := strconv.ParseInt(ctx.URLParam("Page"), 10, 32)
+	if err != nil {
+		page = 0
+	}
+
+	if page > 0 {
+		page -= 1
+	}
+
+	s.Page = page
+
+	pageSize, err := strconv.ParseInt(ctx.URLParam("PageSize"), 10, 32)
+	if err != nil {
+		pageSize = DefaultPageSize
+	}
+
+	s.PageSize = pageSize
+}
+
+func (s *QueryBase) ToQuery(paging bool) *gorm.DB {
+	q := s.db
+
+	q = q.Where("deleted = ?", false)
+
+	if s.PageSize == 0 {
+		s.PageSize = DefaultPageSize
+	}
+
+	if len(s.IDs) > 0 {
+		q = q.Where("id in (?)", s.IDs)
+	}
+
+	if paging {
+		q = q.Limit(int(s.PageSize)).Offset(int(s.Page * s.PageSize))
+	}
+
+	return q
+}
+
+func CreateQueryFromContext(query IQuery, db *gorm.DB, ctx ...iris.Context) IQuery {
+	query.loadDB(db)
+
+	if len(ctx) > 0 {
+		query.fromCtx(ctx[0])
+	}
+
+	return query
+}
--- test/utils_test.go
+++ test/utils_test.go
@@ -2,9 +2,9 @@ package test
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
-	"time"
+
+	"github.com/springmove/sptty"
 )
 
 func TestUID(t *testing.T) {
@@ -13,8 +13,16 @@ func TestUID(t *testing.T) {
 	// 	uids = append(uids, sptty.GenerateUID())
 	// }
 
-	td, _ := time.ParseDuration("24h")
-	var ti interface{} = td
+	// td, _ := time.ParseDuration("24h")
+	// var ti interface{} = td
 
-	fmt.Println(reflect.TypeOf(ti).String() == "time.Duration")
+	// fmt.Println(reflect.TypeOf(ti).String() == "time.Duration")
+
+	// _, mime, err := sptty.GetUrlImage("https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI7YP85icEWiaYmNaYFUrvNdjiaCcFMxO0Wj9dH7QfX5ZtibHPW50HDsibLoHApUic5EALnja3vnn0uKziaQ/132")
+	// if err != nil {
+	// 	fmt.Printf(err.Error())
+	// 	return
+	// }
+
+	fmt.Printf(sptty.Sha1("93102ba73b19e781468b157909274353a8ceb6fa"))
 }
